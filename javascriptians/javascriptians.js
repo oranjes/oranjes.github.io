db34da41e663cff94a36cb86587f4b9e2c94beb9
Danny Engelman <danny@engelman.nl>
2020-09-21 17:47:07 +0200

    datatype 0.6

--- javascriptians/javascriptians.js
+++ javascriptians/javascriptians.js
@@ -1,41 +1,47 @@
-/* Display Bronze, Silver, Gold ranked Javascriptians */
+/* Display 1=Bronze , 2=Zilver, 3=Gold  ranked Javascriptians */
 // FUNCTIONS
 const HTML = {
-  tag: (tag, content, clss = "") => `<${tag} class="${clss}">${content}</${tag}>`,
-  "DIV-tag": ({ content, clss }) => HTML.tag("DIV", content, clss),
+  tag: (tag, content) => `<${tag}>${content}</${tag}>`,
+  "member-tag": ({ content }) => HTML.tag("DIV", content),
 };
 
-const rankArray = (rank) => new Array(HTML.tag("H2", `Rank ${rank}`, "rank")); // ["Level 1", "Lydia", ...]
+const rankArray = (rank) => new Array(HTML.tag("H2", `Rank ${rank}`));
+// return ["<H2>Rank Bronze</H2>"]
+// wordt later: ["<H2>Rank Bronze</H2>", "<DIV class=".member">Lydia</DIV>", ...]
 
-const addMember = (str /* eg. "Lydia,1" */) => {
+function addMember(str /* eg. "Lydia,1" */) {
   const [name, rank = 1] = str.split(","); // 'destructure constants from:' [ "Lydia" , "1" ]
-  const html = HTML["DIV-tag"]({ content: name, clss: "member" });
+  const html = HTML["member-tag"]({ content: name });
   programData.output[rank].push(html);
-};
+}
 
 // PROGRAM
 let testMembers = ["Alexia,3", "Amalia,2", "Ariane"];
 // testMembers = [];
+let ranks = ["Bronze", "Zilver", "Gold"].map(rankArray); // (rank) => rankArray(rank)
 
 const programData = {
-  output: [HTML.tag("H1", "Javascriptians"), rankArray(1), rankArray(2), rankArray(3)],
+  output: [HTML.tag("H1", "Javascriptians"), ...ranks],
   members: ["Lydia,1", "Arnold,1", ...testMembers], // ... is: spread operator
 };
-
+console.log("programData:", programData);
 programData.members.forEach(addMember); // (str) => addMember(str)
+
+// delete programData.members;
+// console.log("programData:", programData);
 
 // ["<H1>Javascriptians</H1>" , [ "Level 1" , "Lydia" .. ] , [ "Level 2" , "JS#2" .. ] ]
 const html = programData.output.flat().join("");
-console.log(html);
-document.body.innerHTML = html;
+console.log("html:", html);
+document.querySelector("DIV").innerHTML = html;
 
 /**
  * Separate parameters versus one Object
  *
  */
 
-//hmm.. okay.. 3 parameters
-const rankArray1 = (rank) => new Array(HTML.tag("H2", `Rank ${rank}`, "rank")); // ["Level 1", "Lydia", ...]
+//hmm.. okay.. 2 parameters
+const rankArray1 = (rank) => new Array(HTML.tag("H2", `Rank ${rank}`)); // ["Bronze", "Lydia", ...]
 
 //maybe better .. 1 parameter Object
 const rankArray2 = (rank) =>
@@ -43,6 +49,7 @@ const rankArray2 = (rank) =>
     HTML.tag({
       tag: "H2",
       content: `Rank ${rank}`,
-      clss: "rank",
+      //className: "rank",
+      //id:"unique ID"
     })
   );
